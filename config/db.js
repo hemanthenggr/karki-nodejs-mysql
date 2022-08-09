a5145a2cdc0d720320ec78037ccdcd232d8693e8
Hemanth Kumar <hemanthenggr@gmail.com>
2022-08-09 13:29:26 +0530

    env variables added

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql')
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'kirki'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 })
 
 module.exports = connection
